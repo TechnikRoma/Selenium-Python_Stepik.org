c1540a03aed84d5c7a37de9e8b35c0928868bce2
Roman <80981369+m1ndkiller@users.noreply.github.com>
2021-05-03 13:12:31 +0300

    Add files via upload

--- 2_1_5 robotsrules.py
+++ 2_1_5 robotsrules.py
@@ -9,15 +9,18 @@ try:
     link = "http://suninjuly.github.io/math.html"
     browser = webdriver.Chrome()
     browser.get(link)
-
+    # находим и копируем значение х из формулы
     x_element = browser.find_element_by_id("input_value")
+    # рассчитываем формулу
     x = x_element.text
     y = calc(x)
+    # находим поле ввода и вставляем ответ 
     input1 = browser.find_element_by_id('answer')
     input1.send_keys(y)
-
+    # отмечаем чекбокс I'm the robot
     option1 = browser.find_element_by_id('robotCheckbox')
     option1.click()
+    # выбираем radiobutton Robots rule
     option2 = browser.find_element_by_id('robotsRule')
     option2.click()
     button = browser.find_element_by_class_name('btn')
